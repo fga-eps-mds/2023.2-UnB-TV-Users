80e91ce1df87677f1f3c15199eceb2f3b289fec7
nYCSTs <lucascandradex@protonmail.com>
2023-10-22 20:52:34 -0300

    Altera database

--- src/database.py
+++ src/database.py
@@ -2,10 +2,12 @@ import os, sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-def get_uri():
-  return os.getenv("TEST_DATABASE_URI") if "pytest" in sys.modules else os.getenv("DATABASE_URI")
+if "pytest" in sys.modules:
+  
+  engine = create_engine(os.getenv("TEST_DATABASE_URI"))
+else:
+  engine = create_engine(f"sqlite:///./{os.getenv('DATABASE_URI', default='users.db')}", connect_args={"check_same_thread": False})
 
-engine = create_engine(get_uri())
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()

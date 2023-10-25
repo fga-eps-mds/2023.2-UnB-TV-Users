db956a15abbf31e2498078c7c332da0e07c70579
lcsAndrade <lucascandradex@protonmail.com>
2023-10-25 18:22:52 -0300

    Atualiza URI do database

--- src/database.py
+++ src/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 if "pytest" in sys.modules:
   engine = create_engine(f"sqlite:///../{os.getenv('TEST_DATABASE_URI', default='unbtv-testing.db')}", connect_args={"check_same_thread": False})  
 else:
-  engine = create_engine(f"sqlite:///../{os.getenv('DATABASE_URI', default='unbtv.db')}", connect_args={"check_same_thread": False})
+  SQLALCHEMY_DATABASE_URL = "postgres://kajfookqkwcfbw:04b047dba3c5c84846aaae6a2bfaa41d455b3c84c08530bd10253f4c806ebffc@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dci6rl2d435oat"
+  engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

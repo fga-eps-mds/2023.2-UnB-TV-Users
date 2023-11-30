a18d62f7a964bba416e0d7cb2d9459afdf904958
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2023-11-30 16:05:03 -0300

    atualizando banco

--- src/model/userModel.py
+++ src/model/userModel.py
@@ -11,11 +11,11 @@ class User(Base):
 
   id = Column(Integer, primary_key=True, index=True)
   name = Column(String, nullable=False)
-  connection = Column(String, nullable=False)
+  connection = Column(String, nullable=True)
   role = Column(String, default="USER")
   email = Column(String, unique=True, index=True, nullable=False)
-  password = Column(String, nullable=False)
-  is_active = Column(Boolean, default=False)
+  password = Column(String, nullable=True)
+  is_active = Column(Boolean, default=True)
   activation_code = Column(Integer, nullable=True)
   password_reset_code = Column(Integer, nullable=True)
 

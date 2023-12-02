d3cf3f23b4fe3d45b1f3554b86c2aa55e93add8a
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2023-12-02 06:59:11 -0300

    Atualiza logica do banco de dados

--- src/model/userModel.py
+++ src/model/userModel.py
@@ -20,17 +20,3 @@ class User(Base):
   password_reset_code = Column(Integer, nullable=True)
 
 
-  social_users = relationship("SocialUser", back_populates="user")
-
-class SocialUser(Base):
-    __tablename__ = "social_users"
-    __table_args__ = {'extend_existing': True}
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    role = Column(String, default="USER")
-
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship("User", back_populates="social_users")
-
